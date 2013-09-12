42adf0cec9d3c7182b525923b918e062e755b9f3
YISONGCMU <yisong0623@gmail.com>
2013-09-11 20:46:13 -0400

    3

---
+++ hw1-yis1/src/main/java/EvaluationAnnotation.java
@@ -0,0 +1,132 @@
+
+
+/* First created by JCasGen Wed Sep 11 20:39:50 EDT 2013 */
+
+import org.apache.uima.jcas.JCas; 
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.jcas.cas.TOP_Type;
+
+import org.apache.uima.jcas.cas.FSArray;
+import org.apache.uima.jcas.cas.FSList;
+
+
+/** Output the Answer by the value of answer score
+and the Precision at N.
+ * Updated by JCasGen Wed Sep 11 20:42:43 EDT 2013
+ * XML source: C:/Users/sy/git/hw1-yis1/hw1-yis1/src/main/resources/hw1-yis1-typesystem.xml
+ * @generated */
+public class EvaluationAnnotation extends FSList {
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = JCasRegistry.register(EvaluationAnnotation.class);
+  /** @generated
+   * @ordered 
+   */
+  @SuppressWarnings ("hiding")
+  public final static int type = typeIndexID;
+  /** @generated  */
+  @Override
+  public              int getTypeIndexID() {return typeIndexID;}
+ 
+  /** Never called.  Disable default constructor
+   * @generated */
+  protected EvaluationAnnotation() {/* intentionally empty block */}
+    
+  /** Internal - constructor used by generator 
+   * @generated */
+  public EvaluationAnnotation(int addr, TOP_Type type) {
+    super(addr, type);
+    readObject();
+  }
+  
+  /** @generated */
+  public EvaluationAnnotation(JCas jcas) {
+    super(jcas);
+    readObject();   
+  } 
+
+  /** <!-- begin-user-doc -->
+    * Write your own initialization here
+    * <!-- end-user-doc -->
+  @generated modifiable */
+  private void readObject() {/*default - does nothing empty block */}
+     
+ 
+    
+  //*--------------*
+  //* Feature: Precision
+
+  /** getter for Precision - gets The answer score precision at N (where N is the total number of correct answers)
+   * @generated */
+  public double getPrecision() {
+    if (EvaluationAnnotation_Type.featOkTst && ((EvaluationAnnotation_Type)jcasType).casFeat_Precision == null)
+      jcasType.jcas.throwFeatMissing("Precision", "EvaluationAnnotation");
+    return jcasType.ll_cas.ll_getDoubleValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_Precision);}
+    
+  /** setter for Precision - sets The answer score precision at N (where N is the total number of correct answers) 
+   * @generated */
+  public void setPrecision(double v) {
+    if (EvaluationAnnotation_Type.featOkTst && ((EvaluationAnnotation_Type)jcasType).casFeat_Precision == null)
+      jcasType.jcas.throwFeatMissing("Precision", "EvaluationAnnotation");
+    jcasType.ll_cas.ll_setDoubleValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_Precision, v);}    
+   
+    
+  //*--------------*
+  //* Feature: AnswerNumberN
+
+  /** getter for AnswerNumberN - gets Total answer number.
+   * @generated */
+  public int getAnswerNumberN() {
+    if (EvaluationAnnotation_Type.featOkTst && ((EvaluationAnnotation_Type)jcasType).casFeat_AnswerNumberN == null)
+      jcasType.jcas.throwFeatMissing("AnswerNumberN", "EvaluationAnnotation");
+    return jcasType.ll_cas.ll_getIntValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_AnswerNumberN);}
+    
+  /** setter for AnswerNumberN - sets Total answer number. 
+   * @generated */
+  public void setAnswerNumberN(int v) {
+    if (EvaluationAnnotation_Type.featOkTst && ((EvaluationAnnotation_Type)jcasType).casFeat_AnswerNumberN == null)
+      jcasType.jcas.throwFeatMissing("AnswerNumberN", "EvaluationAnnotation");
+    jcasType.ll_cas.ll_setIntValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_AnswerNumberN, v);}    
+   
+    
+  //*--------------*
+  //* Feature: elements
+
+  /** getter for elements - gets the output of the evaluation step.
+Answer Annotations are ranked by the answer score. 
+   * @generated */
+  public FSArray getElements() {
+    if (EvaluationAnnotation_Type.featOkTst && ((EvaluationAnnotation_Type)jcasType).casFeat_elements == null)
+      jcasType.jcas.throwFeatMissing("elements", "EvaluationAnnotation");
+    return (FSArray)(jcasType.ll_cas.ll_getFSForRef(jcasType.ll_cas.ll_getRefValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_elements)));}
+    
+  /** setter for elements - sets the output of the evaluation step.
+Answer Annotations are ranked by the answer score.  
+   * @generated */
+  public void setElements(FSArray v) {
+    if (EvaluationAnnotation_Type.featOkTst && ((EvaluationAnnotation_Type)jcasType).casFeat_elements == null)
+      jcasType.jcas.throwFeatMissing("elements", "EvaluationAnnotation");
+    jcasType.ll_cas.ll_setRefValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_elements, jcasType.ll_cas.ll_getFSRef(v));}    
+    
+  /** indexed getter for elements - gets an indexed value - the output of the evaluation step.
+Answer Annotations are ranked by the answer score. 
+   * @generated */
+  public AnswerScoreAnnotation getElements(int i) {
+    if (EvaluationAnnotation_Type.featOkTst && ((EvaluationAnnotation_Type)jcasType).casFeat_elements == null)
+      jcasType.jcas.throwFeatMissing("elements", "EvaluationAnnotation");
+    jcasType.jcas.checkArrayBounds(jcasType.ll_cas.ll_getRefValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_elements), i);
+    return (AnswerScoreAnnotation)(jcasType.ll_cas.ll_getFSForRef(jcasType.ll_cas.ll_getRefArrayValue(jcasType.ll_cas.ll_getRefValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_elements), i)));}
+
+  /** indexed setter for elements - sets an indexed value - the output of the evaluation step.
+Answer Annotations are ranked by the answer score. 
+   * @generated */
+  public void setElements(int i, AnswerScoreAnnotation v) { 
+    if (EvaluationAnnotation_Type.featOkTst && ((EvaluationAnnotation_Type)jcasType).casFeat_elements == null)
+      jcasType.jcas.throwFeatMissing("elements", "EvaluationAnnotation");
+    jcasType.jcas.checkArrayBounds(jcasType.ll_cas.ll_getRefValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_elements), i);
+    jcasType.ll_cas.ll_setRefArrayValue(jcasType.ll_cas.ll_getRefValue(addr, ((EvaluationAnnotation_Type)jcasType).casFeatCode_elements), i, jcasType.ll_cas.ll_getFSRef(v));}
+  }
+
+    
---
+++ hw1-yis1/src/main/java/EvaluationAnnotation_Type.java
@@ -0,0 +1,145 @@
+
+/* First created by JCasGen Wed Sep 11 20:39:50 EDT 2013 */
+
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.JCasRegistry;
+import org.apache.uima.cas.impl.CASImpl;
+import org.apache.uima.cas.impl.FSGenerator;
+import org.apache.uima.cas.FeatureStructure;
+import org.apache.uima.cas.impl.TypeImpl;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.impl.FeatureImpl;
+import org.apache.uima.cas.Feature;
+import org.apache.uima.jcas.cas.FSList_Type;
+
+/** Output the Answer by the value of answer score
+and the Precision at N.
+ * Updated by JCasGen Wed Sep 11 20:42:43 EDT 2013
+ * @generated */
+public class EvaluationAnnotation_Type extends FSList_Type {
+  /** @generated */
+  @Override
+  protected FSGenerator getFSGenerator() {return fsGenerator;}
+  /** @generated */
+  private final FSGenerator fsGenerator = 
+    new FSGenerator() {
+      public FeatureStructure createFS(int addr, CASImpl cas) {
+  			 if (EvaluationAnnotation_Type.this.useExistingInstance) {
+  			   // Return eq fs instance if already created
+  		     FeatureStructure fs = EvaluationAnnotation_Type.this.jcas.getJfsFromCaddr(addr);
+  		     if (null == fs) {
+  		       fs = new EvaluationAnnotation(addr, EvaluationAnnotation_Type.this);
+  			   EvaluationAnnotation_Type.this.jcas.putJfsFromCaddr(addr, fs);
+  			   return fs;
+  		     }
+  		     return fs;
+        } else return new EvaluationAnnotation(addr, EvaluationAnnotation_Type.this);
+  	  }
+    };
+  /** @generated */
+  @SuppressWarnings ("hiding")
+  public final static int typeIndexID = EvaluationAnnotation.typeIndexID;
+  /** @generated 
+     @modifiable */
+  @SuppressWarnings ("hiding")
+  public final static boolean featOkTst = JCasRegistry.getFeatOkTst("EvaluationAnnotation");
+ 
+  /** @generated */
+  final Feature casFeat_Precision;
+  /** @generated */
+  final int     casFeatCode_Precision;
+  /** @generated */ 
+  public double getPrecision(int addr) {
+        if (featOkTst && casFeat_Precision == null)
+      jcas.throwFeatMissing("Precision", "EvaluationAnnotation");
+    return ll_cas.ll_getDoubleValue(addr, casFeatCode_Precision);
+  }
+  /** @generated */    
+  public void setPrecision(int addr, double v) {
+        if (featOkTst && casFeat_Precision == null)
+      jcas.throwFeatMissing("Precision", "EvaluationAnnotation");
+    ll_cas.ll_setDoubleValue(addr, casFeatCode_Precision, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_AnswerNumberN;
+  /** @generated */
+  final int     casFeatCode_AnswerNumberN;
+  /** @generated */ 
+  public int getAnswerNumberN(int addr) {
+        if (featOkTst && casFeat_AnswerNumberN == null)
+      jcas.throwFeatMissing("AnswerNumberN", "EvaluationAnnotation");
+    return ll_cas.ll_getIntValue(addr, casFeatCode_AnswerNumberN);
+  }
+  /** @generated */    
+  public void setAnswerNumberN(int addr, int v) {
+        if (featOkTst && casFeat_AnswerNumberN == null)
+      jcas.throwFeatMissing("AnswerNumberN", "EvaluationAnnotation");
+    ll_cas.ll_setIntValue(addr, casFeatCode_AnswerNumberN, v);}
+    
+  
+ 
+  /** @generated */
+  final Feature casFeat_elements;
+  /** @generated */
+  final int     casFeatCode_elements;
+  /** @generated */ 
+  public int getElements(int addr) {
+        if (featOkTst && casFeat_elements == null)
+      jcas.throwFeatMissing("elements", "EvaluationAnnotation");
+    return ll_cas.ll_getRefValue(addr, casFeatCode_elements);
+  }
+  /** @generated */    
+  public void setElements(int addr, int v) {
+        if (featOkTst && casFeat_elements == null)
+      jcas.throwFeatMissing("elements", "EvaluationAnnotation");
+    ll_cas.ll_setRefValue(addr, casFeatCode_elements, v);}
+    
+   /** @generated */
+  public int getElements(int addr, int i) {
+        if (featOkTst && casFeat_elements == null)
+      jcas.throwFeatMissing("elements", "EvaluationAnnotation");
+    if (lowLevelTypeChecks)
+      return ll_cas.ll_getRefArrayValue(ll_cas.ll_getRefValue(addr, casFeatCode_elements), i, true);
+    jcas.checkArrayBounds(ll_cas.ll_getRefValue(addr, casFeatCode_elements), i);
+  return ll_cas.ll_getRefArrayValue(ll_cas.ll_getRefValue(addr, casFeatCode_elements), i);
+  }
+   
+  /** @generated */ 
+  public void setElements(int addr, int i, int v) {
+        if (featOkTst && casFeat_elements == null)
+      jcas.throwFeatMissing("elements", "EvaluationAnnotation");
+    if (lowLevelTypeChecks)
+      ll_cas.ll_setRefArrayValue(ll_cas.ll_getRefValue(addr, casFeatCode_elements), i, v, true);
+    jcas.checkArrayBounds(ll_cas.ll_getRefValue(addr, casFeatCode_elements), i);
+    ll_cas.ll_setRefArrayValue(ll_cas.ll_getRefValue(addr, casFeatCode_elements), i, v);
+  }
+ 
+
+
+
+  /** initialize variables to correspond with Cas Type and Features
+	* @generated */
+  public EvaluationAnnotation_Type(JCas jcas, Type casType) {
+    super(jcas, casType);
+    casImpl.getFSClassRegistry().addGeneratorForType((TypeImpl)this.casType, getFSGenerator());
+
+ 
+    casFeat_Precision = jcas.getRequiredFeatureDE(casType, "Precision", "uima.cas.Double", featOkTst);
+    casFeatCode_Precision  = (null == casFeat_Precision) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_Precision).getCode();
+
+ 
+    casFeat_AnswerNumberN = jcas.getRequiredFeatureDE(casType, "AnswerNumberN", "uima.cas.Integer", featOkTst);
+    casFeatCode_AnswerNumberN  = (null == casFeat_AnswerNumberN) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_AnswerNumberN).getCode();
+
+ 
+    casFeat_elements = jcas.getRequiredFeatureDE(casType, "elements", "uima.cas.FSArray", featOkTst);
+    casFeatCode_elements  = (null == casFeat_elements) ? JCas.INVALID_FEATURE_CODE : ((FeatureImpl)casFeat_elements).getCode();
+
+  }
+}
+
+
+
+    
